ea680313fa20c14dd876e58820c6b14c5c668401
ta1ilz <24638vl@hvhs.school.nz>
2026-07-28 07:55:51 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+<script>
+    let my_donuts = [];
+    let till_total = 0;
 
+    const visualList = document.getElementById('displayBox');
+    const cashField = document.getElementById('custMoney');
+
+    function grabItem(chosenName, itemCost) {
+        ...
+    }
+
+    function updateCartDisplay() {
+        ...
+    }
+
+    function runTotal() {
+        ...
+    }
+
+    function clearCart() {
+        ...
+    }
+
+    function buildCheckoutList() {
+        ...
+    }
+
+    function sendOrder(e) {
+        ...
+    }
+</script>
